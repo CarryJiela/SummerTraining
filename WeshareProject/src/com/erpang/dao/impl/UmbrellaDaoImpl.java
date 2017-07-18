e590be6989ba25daf76381bfb3a5da1a56e483df
RebornHugo <kiss152535@gmail.com>
2017-07-18 18:20:35 +0800

    Update UmbrellaDaoImpl.java
    finish 5 functions!

--- WeshareProject/src/com/erpang/dao/impl/UmbrellaDaoImpl.java
+++ WeshareProject/src/com/erpang/dao/impl/UmbrellaDaoImpl.java
@@ -3,16 +3,22 @@ package com.erpang.dao.impl;
 import com.erpang.dao.IUmbrellaDao;
 import com.erpang.vo.Record;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.Date;
 import java.util.List;
 
 /**
  * Created by hugo on 17-7-18.
  */
-public class UmbrellaDaoImpl extends IUmbrellaDao{
+public class UmbrellaDaoImpl implements IUmbrellaDao {
+
+    private Connection conn = null;
+    private PreparedStatement ps = null;
+
     @Override
     public boolean Increase(Record rec) throws Exception {
-        
+
 
         return false;
     }
@@ -43,27 +49,69 @@ public class UmbrellaDaoImpl extends IUmbrellaDao{
     }
 
     @Override
-    public boolean changeStartTime(String uno, int start_time) throws Exception {
-        return false;
+    public boolean changeStartTime(String uno, Date start_time) throws Exception {
+        boolean flag = false;
+        String sql = "UPDATE unfinished SET start_time = ? WHERE uno = ? ";
+        this.ps = this.conn.prepareStatement(sql);
+        this.ps.setTime(1, new java.sql.Time(start_time.getTime()));
+        this.ps.setString(2, uno);
+        if (this.ps.executeUpdate() > 0) {
+            flag = true;
+        }
+        this.ps.close();
+        return flag;
     }
 
     @Override
-    public boolean changeEndtime(String uno, int end_time) throws Exception {
-        return false;
+    public boolean changeEndtime(String uno, Date end_time) throws Exception {
+        boolean flag = false;
+        String sql = "UPDATE unfinished SET end_time = ? WHERE uno = ? ";
+        this.ps = this.conn.prepareStatement(sql);
+        this.ps.setTime(1,  new java.sql.Time(end_time.getTime()));
+        this.ps.setString(2, uno);
+        if (this.ps.executeUpdate() > 0) {
+            flag = true;
+        }
+        this.ps.close();
+        return flag;
     }
 
     @Override
     public boolean changeDate(String uno, Date date_r) throws Exception {
-        return false;
+        boolean flag = false;
+        String sql = "UPDATE unfinished SET date_r = ? WHERE uno = ?";
+        this.ps = this.conn.prepareStatement(sql);
+        this.ps.setDate(1, new java.sql.Date(date_r.getTime()));
+        this.ps.setString(2,uno);
+        if (this.ps.executeUpdate() > 0) {
+            flag = true;
+        }
+        return flag;
     }
 
     @Override
-    public boolean changeEndPoint(String uno, boolean have) throws Exception {
-        return false;
+    public boolean changeHave(String uno, boolean have) throws Exception {
+        boolean flag = false;
+        String sql = "UPDATE unfinished SET have = ? WHERE uno = ?";
+        this.ps = this.conn.prepareStatement(sql);
+        this.ps.setBoolean(1, have);
+        this.ps.setString(2,uno);
+        if (this.ps.executeUpdate() > 0) {
+            flag = true;
+        }
+        return flag;
     }
 
     @Override
     public boolean changeRemark(String uno, String remark) throws Exception {
-        return false;
+        boolean flag = false;
+        String sql = "UPDATE unfinished SET remark = ? WHERE uno = ?";
+        this.ps = this.conn.prepareStatement(sql);
+        this.ps.setString(1, remark);
+        this.ps.setString(2,uno);
+        if (this.ps.executeUpdate() > 0) {
+            flag = true;
+        }
+        return flag;
     }
 }
